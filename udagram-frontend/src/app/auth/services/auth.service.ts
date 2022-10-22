7dd44d02b6c1c7e7a6c16a931fb1f27a4577f3ce
Hoang95vn <kaiser95vn@gmail.com>
2022-10-22 12:20:54 +0700

    set up project

---
+++ udagram-frontend/src/app/auth/services/auth.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { ApiService } from 'src/app/api/api.service';
+import { catchError, tap } from 'rxjs/operators';
+
+const JWT_LOCALSTORE_KEY = 'jwt';
+const USER_LOCALSTORE_KEY = 'user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  currentUser$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+  constructor( private api: ApiService ) {
+    this.initToken();
+  }
+
+  initToken() {
+    const token = localStorage.getItem(JWT_LOCALSTORE_KEY);
+    const user = <User> JSON.parse(localStorage.getItem(USER_LOCALSTORE_KEY));
+    if (token && user) {
+      this.setTokenAndUser(token, user);
+    }
+  }
+
+  setTokenAndUser(token: string, user: User) {
+    console.log(`Localstorage Token key ${JWT_LOCALSTORE_KEY} | value ${token}`);
+    localStorage.setItem(JWT_LOCALSTORE_KEY, token);
+    localStorage.setItem(USER_LOCALSTORE_KEY, JSON.stringify(user));
+    this.api.setAuthToken(token);
+    this.currentUser$.next(user);
+  }
+
+  async login(email: string, password: string): Promise<any> {
+    return this.api.post('/users/auth/login',
+              {email: email, password: password})
+              .then((res) => {
+                this.setTokenAndUser(res.token, res.user);
+                return res;
+              })
+              .catch((e) => { throw e; });
+      // return user !== undefined;
+  }
+
+  logout(): boolean {
+    this.setTokenAndUser(null, null);
+    return true;
+  }
+
+  register(user: User, password: string): Promise<any> {
+    return this.api.post('/users/auth/',
+              {email: user.email, password: password})
+              .then((res) => {
+                console.log(`setting token after registration: ${res.token}`)
+                this.setTokenAndUser(res.token, res.user);
+                return res;
+              })
+              .catch((e) => { throw e; });
+  }
+}

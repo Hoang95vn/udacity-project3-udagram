9aeec93c3cd31866bd15e2ae4c52b96911edc044
Hoang95vn <kaiser95vn@gmail.com>
2022-10-22 22:46:57 +0700

    update deploy kube + screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8fff649ee1f94c1890e300d767d0c89-1301663682.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
